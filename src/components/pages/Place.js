fbe8b935495e4da5aa59447f20145357f4b67f3c
Marissa Beatty <MarissaBeatty@users.noreply.github.com>
2017-08-18 23:10:25 -0400

    implemented warning message when user submits empty search form

--- src/components/pages/Place.js
+++ src/components/pages/Place.js
@@ -86,7 +86,7 @@ export default class Place extends Component {
       <div className="placePage">
           <br></br>
           <div className="test">  
-            <div className="map-container">
+            <div className="map-container" div key={idx}>
               {initialCenter !== {} ? <MapContainer bitesInfo={bite} initialCenter={initialCenter} /> : null}  
             </div>
           </div>
