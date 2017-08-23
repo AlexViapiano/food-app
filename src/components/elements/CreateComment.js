3403797b9b079ee91d8445a217393c2142ec23f5
Marissa Beatty <MarissaBeatty@users.noreply.github.com>
2017-08-23 18:03:22 -0400

    comment button diabled and textarea clears after submit

--- src/components/elements/CreateComment.js
+++ src/components/elements/CreateComment.js
@@ -6,17 +6,26 @@ export default class CreateComment extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isCommenting: false
     };
   }
 
   _handleCreateComment = () => {
   	let comment = this.refs.comment.value;
-  	this.props.onPostComment(comment)
+  	this.props.onPostComment(comment);
+    this.setState({
+      isCommenting: true
+    });
+    this.refs.comment.value = "";
   }
 
+  _handleTyping = () => {
+    this.setState({
+      isCommenting: false
+    });
+  }
 
   render() {
-
     return (
         <div className="comment-area">
           <textarea 
@@ -25,9 +34,12 @@ export default class CreateComment extends Component {
             maxLength="400"
             rows="8"
             cols="50"
+            onKeyUp={this._handleTyping}
             />
 	        <br></br>
-          <button onClick={this._handleCreateComment}>Create Comment</button>
+          {this.state.isCommenting === false ?
+          <button onClick={this._handleCreateComment}>Create Comment</button> 
+          : null }
       	</div>
     )
   }
