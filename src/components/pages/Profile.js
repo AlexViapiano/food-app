8040d65c36acca5fcd6f9df46229d18982918f36
Alex <alexviapiano@hotmail.com>
2017-08-23 22:32:36 -0400

    Added a profile page under construction

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import api from '../../api';
+//import api from '../../api';
 import auth from '../../auth';
 import './Profile.css';
 
@@ -8,20 +8,8 @@ export default class Profile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // profile: {}
     };
 }
-
-
-componentWillMount() {
-    // api.getProfile()
-    // .then(res => {
-    //       this.setState({ 
-    //            profile: res.body
-    //       })
-    // })
-}
-
 
   render() {
     const isLoggedIn = auth.isLoggedIn();
